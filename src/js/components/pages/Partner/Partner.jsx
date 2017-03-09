bde24b5fcc167dcbe31808e4169ab4af4ea92475
Jason Rothdiener <jason_ro@hotmail.com>
2017-03-09 18:34:15 +0900

    Added Partner Page

---
+++ src/js/components/pages/Partner/Partner.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+const React = require('react');
+
+import PageHeader from '../../shared/PageHeader.jsx';
+
+class Partner extends React.Component{
+  render() {
+    return (
+      <div>
+        <PageHeader />
+
+      </div>
+    );
+  }
+};
+
+module.exports = Partner;
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Router, Route, browserHistory, DefaultRoute, NotFoundRoute, Redirect } from 'react-router'
 const HomePage = require('./components/pages/Home.jsx');
+const Partner = require('./components/pages/Partner/Partner.jsx');
 
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={HomePage} />
+    <Route path="/partner/" component={Partner} />
   </Router>
 );
 
