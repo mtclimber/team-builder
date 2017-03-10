da70747dc4f6f7380cc8bbf69568736df869fade
DESKTOP-CS9FHBO\TulsaTankersleys <ry.tankersley@gmail.com>
2017-03-10 12:47:18 +0900

    Changed the name away from dashboard

--- src/js/api/ChurchApi.jsx
+++ src/js/api/ChurchApi.jsx
@@ -2,9 +2,17 @@ const $ = require('jquery');
 
 let stations = null;
 class Church {
-
+  static getPartners(cb) {
+    $.get(`/api/partners`)
+    .done((response) => {
+      cb(response);
+    })
+    .fail((response) => {
+      cb(response);
+    });
+  };
+  
   static addChurchContact(audienceType, interactionType, notes, cb) {
-    console.log(`${audienceType} ${interactionType} ${notes}`);
     $.post(`/api/church/contact`, {
       audienceType: audienceType,
       interactionType: interactionType,
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Router, Route, browserHistory, DefaultRoute, NotFoundRoute, Redirect } from 'react-router'
-const Dashboard = require('./components/pages/Dashboard/Dashboard.jsx');
+const ChurchViewer = require('./components/pages/ChurchViewer/ChurchViewer.jsx');
 const Partner = require('./components/pages/Partner/Partner.jsx');
 const Report = require('./components/pages/Report/Report.jsx');
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Dashboard} />
+    <Route path="/" component={ChurchViewer} />
     <Route path="/partner" component={Partner} />
     <Route path="/report" component={Report} />
   </Router>
